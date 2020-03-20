3c28c2e28e241123b6503c05f49828dbee5745e3
kumarvimlesh <kumarvimlesh9889@gmail.com>
2020-03-20 16:51:22 +0530

    styling finish

--- fetchdata.js
+++ fetchdata.js
@@ -15,16 +15,24 @@ async function fetchdata(){
     document.getElementById("geo-location").innerHTML="Latitude : "+json.data.city.geo[0]+" Longitude : "+json.data.city.geo[1];
     document.getElementById("time").innerHTML="Last Updated : "+json.data.time.s;
     document.getElementById("aqi").innerHTML="Air Quality Index : "+json.data.aqi;
+    document.getElementById("dominent").innerHTML="Dominent Polution : "+json.data.dominentpol;
     //alert(json.data.city.name);
     document.getElementById("result").innerHTML=" ";
 
-    var data=json.data.iaqi;
-
-    for(var k in data){
+    var api_data=json.data.iaqi;
+    
+    var arr_data=[];
+    for(var k in api_data){
     	//console.log(k);
     	//console.log(k+" = "+data[k].v);
     	var list = document.createElement("li");
-        list.innerHTML=k+" = "+data[k].v;
+        list.innerHTML=k+" = "+api_data[k].v;
+        arr_data.push([k+":"+api_data[k].v])
+        //alert(arr_data);
         document.getElementById("result").appendChild(list);
     }
+
+
+    console.log(arr_data);
+    
 }
